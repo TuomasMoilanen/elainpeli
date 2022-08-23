35c8d7dc8bdf5dfef79f83f0fe07a9448941290b
TuomasMoilanen <35306402+TuomasMoilanen@users.noreply.github.com>
2022-08-23 13:24:36 +0300

    Take damage from dingoes and pass through them.

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -17,8 +17,19 @@ public class HealthScript : MonoBehaviour
     {
 
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            takeDamage(20);
+        }
+    }
     public void GainHealth(int healAmount)
     {
         playerHealth = playerHealth + healAmount;
+    }
+    void takeDamage(int damage)
+    {
+        playerHealth = playerHealth - damage;
     }
 }
